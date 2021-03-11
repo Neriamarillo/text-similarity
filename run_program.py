fbcd8c3f100f05942fcd1893bb451248041122fe
Neriamarillo <jorge.e.nieves@gmail.com>
2021-03-11 17:30:34 -0600

    Fix - Code Cleanup, Error Handling
    ~ Cleanup Code
    + Add Error Handling for empty text values

--- run_program.py
+++ run_program.py
@@ -8,6 +8,35 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+def load_stopwords():
+    # Init stopwords
+    stopwords = []
+    with open('stopwords.txt', 'r') as file:
+        for word in file.read().splitlines():
+            stopwords.append(word)
+    return stopwords
+
+
+def clean_stopwords(text):
+    # Remove stopwords
+    stopwords = load_stopwords()
+    clean_result = ' '.join([ele.lower() for ele in text.split() if ele not in stopwords])
+    return clean_result
+
+
+def remove_punctuations(text):
+    # Remove punctuations
+    result = ''.join([ele for ele in text if ele not in string.punctuation])
+    return result
+
+
+def compute_similarity(text_1, text_2):
+    if not text_1 and not text_2:
+        return "Fields were empty. No score to show."
+    similarity = float(len(text_1.intersection(text_2)) / len(text_1.union(text_2)))
+    return 'Similarity Score: {0:.2f}'.format(similarity)
+
+
 @app.route("/", methods=['GET'])
 def home():
     return render_template("index.html")
@@ -19,35 +48,14 @@ def check_similarity():
     Jaccard Similarity. Utilize sets to find the words that are common in both samples of text (intersection)
     and the ones who are in either one (union).
     """
-
-    # Init stopwords
-    stopwords = []
-    with open('stopwords.txt', 'r') as file:
-        for word in file.read().splitlines():
-            stopwords.append(word)
-
     # Values from POST body
-    text_1 = request.form["text_1"]
-    text_2 = request.form["text_2"]
+    text_1 = request.form["text_1"] if request.form["text_1"] else ''
+    text_2 = request.form["text_2"] if request.form["text_2"] else ''
 
-    # Remove stopwords
-    a_ = ' '.join([ele.lower() for ele in text_1.split() if ele not in stopwords])
-    b_ = ' '.join([ele.lower() for ele in text_2.split() if ele not in stopwords])
+    clean_text_1 = set(remove_punctuations(clean_stopwords(text_1)).split())
+    clean_text_2 = set(remove_punctuations(clean_stopwords(text_2)).split())
 
-    # Remove punctuations
-    a__ = ''.join([ele for ele in a_ if ele not in string.punctuation])
-    b__ = ''.join([ele for ele in b_ if ele not in string.punctuation])
-
-    # Create word set
-    a = set(a__.split())
-    b = set(b__.split())
-
-    # Compute similarity
-    similarity = float(len(a.intersection(b)) / len(a.union(b)))
-
-    print('Similarity Score: {0:.2f} '.format(similarity))
-
-    return '<h1>Similarity Score: {0:.2f}</h1>'.format(similarity)
+    return '<h1>{}</h1>'.format(compute_similarity(clean_text_1, clean_text_2))
 
 
 if __name__ == "__main__":
